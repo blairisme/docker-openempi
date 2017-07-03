2a0bdfec35c94094089763ff0ef9be1af0c751db
Yuan <yw.johnny@gmail.com>
2017-07-03 17:47:23 +0100

    Memory Issue Fixes

--- application/setenv.sh
+++ application/setenv.sh
@@ -2,7 +2,7 @@
 # Define environment variables required for Tomcat
 
 export OPENEMPI_HOME=/opt/openempi-3.3.0
-export CATALINA_OPTS="-Xms512m -Xmx4096m -XX:+UseG1GC \
+export CATALINA_OPTS="-Xms128m -Xmx1024m -XX:+UseG1GC \
                       -XX:MaxDirectMemorySize=1999m \
                       -Dopenempi.home=$OPENEMPI_HOME \
                       -Djdbc.url=jdbc\:postgresql\://postgres:5432/openempi"
